0f76003199ffc5a734500f3c96cb3b5b268f0d4e
Oleksandr Kriznij <oleksandrkriznij200@gmail.com>
2026-02-09 14:41:40 +0000

    first commit

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,34 @@
-:
+#include <stdio.h>
+
+struct Product {
+    char name[100];
+    float price;
+    int quantity;
+};
+
+int main() {
+    struct Product product;
+    float total;
+
+    printf("Enter name: ");
+    fgets(product.name, sizeof(product.name), stdin);
+
+    printf("Enter price: ");
+    scanf("%f", &product.price);
+
+    printf("Enter quantity: ");
+    scanf("%d", &product.quantity);
+
+    for (int i = 0; product.name[i]; i++) {
+        if (product.name[i] == '\n') {
+            product.name[i] = '\0';
+            break;
+        }
+    }
+
+    total = product.price * product.quantity;
+
+    printf("Total: %.2f UAH\n", total);
+
+    return 0;
+}
